42f795e34ed35056aed4d529c4136b411477f5a4
Ansel P <ansel.panicker@slu.edu>
2024-10-03 09:15:27 -0500

    key_gen

--- key_scheduler.cpp
+++ key_scheduler.cpp
@@ -18,9 +18,41 @@ sub_key[n-1] = temp;
     return sub_key;
 } 
 
-vector<string> key_gen(string key){
+// Shift schedule for each round
+int SHIFT_SCHEDULE[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+
+string PC1Apply(string key) {
+    string permuted_key = "";
+    for (int i = 0; i < 56; i++) {
+        permuted_key += key[PC1[i] - 1];
+    }
+    return permuted_key;
+}
+
+
+string PC2Apply(string combined) {
+    string sub_key = "";
+    for (int i = 0; i < 48; i++) {
+        sub_key += combined[PC2[i] - 1];  
+    }
+    return sub_key;
+}
+
+vector<string> key_gen(string key) {
     vector<string> sub_keys;
-    //write code from here
-    
+    string permuted_key = PC1Apply(key);
+    string C = permuted_key.substr(0, 28);
+    string D = permuted_key.substr(28, 28);
+
+    for (int round = 0; round < 16; round++) {
+        for (int shift = 0; shift < SHIFT_SCHEDULE[round]; shift++) {
+            C = shift_left(C);
+            D = shift_left(D);
+        string combined = C + D;
+        string sub_key = PC2Apply(combined);
+        sub_keys.push_back(sub_key);
+    }
+
     return sub_keys;
+}
 }
